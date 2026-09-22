35eb953dc93f4f7ab4f8895aa0001bc7429ea1d4
itsafal <141961256+itsafal@users.noreply.github.com>
2026-09-21 22:22:55 -0400

    style: remove trailing blank line from client exports

--- sdk/typescript/src/index.ts
+++ sdk/typescript/src/index.ts
@@ -3,4 +3,3 @@ export { ClientError, RangeChunk, stringify, type JsonValue } from './wire.js';
 export { EventStream, type Event } from './stream.js';
 export type { RequestOptions } from './transport.js';
 export type * from './models.js';
-
